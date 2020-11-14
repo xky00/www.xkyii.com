9ac928229125c601503504ed077b52e2667c40e8
xkyii <vkyii@foxmail.com>
2020-11-15 00:39:40 +0800

    Up：为了vs自动提示

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace WebGenerator
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .SetThemePath("theme")
+                .AddSetting(WebKeys.InputPaths, new [] { "docs" })
+                .AddSetting(WebKeys.ExcludedPaths, new List<NormalizedPath>
+                {
+                    new NormalizedPath(".github"),
+                    new NormalizedPath(".git"),
+                })
+                .RunAsync();
+        }
+    }
+}
