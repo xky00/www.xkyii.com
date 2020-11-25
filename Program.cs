9e7feb2c494f0dd6bbb07337f90537c39140e27f
xkyii <vkyii@foxmail.com>
2020-11-25 17:42:42 +0800

    Add: Image url rebase

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Statiq.App;
 using Statiq.Common;
+using Statiq.Markdown;
 using Statiq.Web;
+using WebGenerator.Extensions;
 
 namespace WebGenerator
 {
@@ -14,12 +15,25 @@ namespace WebGenerator
             return await Bootstrapper
                 .Factory
                 .CreateWeb(args)
+                .ConfigureTemplates(templates =>
+                {
+                    if (templates.ContainsKeys(MediaTypes.Markdown))
+                    {
+                        templates.Remove(MediaTypes.Markdown);
+
+                        var render = new RenderMarkdown()
+                            .UseExtensions()
+                            .UseExtension<ImageUrlMarkdownExtension>();
+                        templates.Add(MediaTypes.Markdown, new Template(ContentType.Content, Phase.Process, render));
+                    }
+                })
                 .SetThemePath("theme")
                 .AddSetting(WebKeys.InputPaths, new [] { "docs" })
                 .AddSetting(WebKeys.ExcludedPaths, new List<NormalizedPath>
                 {
                     new NormalizedPath(".github"),
                     new NormalizedPath(".git"),
+                    new NormalizedPath("assets/image1/.git"),
                 })
                 .RunAsync();
         }
--- docs/bookmark/main-bookmark.md
+++ docs/bookmark/main-bookmark.md
@@ -1,10 +1,11 @@
 ---
 title: 书签
 layout: post
+ImageRootUrl: https://image1.xkyii.com/
 DestinationFileName: index.html
 ---
 
 ## 软件
 * [Scoop](https://scoop.sh/)
 
-
+![image](../assets/image1/2020/whitelabel-error.png)
